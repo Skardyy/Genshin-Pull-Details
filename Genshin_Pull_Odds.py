348858bb331997007ddb94b077cfa74e6d671006
Meron Bossin <meronb1312@gmail.com>
2023-04-01 03:26:09 +0300

    removed the plot, for easier result fetching

--- Genshin_Pull_Odds.py
+++ Genshin_Pull_Odds.py
@@ -19,24 +19,18 @@ plt.subplot(3,1,1)
 plt.title('Odds to receive 5 star in a single pull')
 plt.scatter(df['SingleTrue'].index, df['SingleTrue'],
             label = 'SingleTrue', color = 'g')
-plt.plot(df['SingleTrue'].index, df['SingleTrue'],
-         color = 'g')
 plt.legend()
 
 plt.subplot(3,1,2)
 plt.title('Odds to not receive 5 star in a single pull')
 plt.scatter(df['SingleFalse'].index, df['SingleFalse'],
             label = 'SingleFalse', color = 'r')
-plt.plot(df['SingleFalse'].index, df['SingleFalse'],
-         color = 'r')
 plt.legend()
 
 plt.subplot(3,1,3)
 plt.title('Odds to not receive 5 star in cumlative pulls')
 plt.scatter(df['CumProd'].index, df['CumProd'],
             label = 'CumProd', color = 'b')
-plt.plot(df['CumProd'].index, df['CumProd'],
-         color = 'b')
 plt.legend()
 
 mplcursors.cursor(hover=True)
